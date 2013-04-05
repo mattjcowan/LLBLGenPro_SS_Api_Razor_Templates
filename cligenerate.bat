4b9f0d62a7a8462fac4d5653106d29b0b8b9d633
Matt Cowan <matthewj.cowan@gmail.com>
2013-04-04 20:07:45 -0500

    Removed cligenerate.* files from git src control

--- cligenerate.bat
+++
@@ -1,3 +0,0 @@
-echo In order to use this, you have to compile the CliGenerator source from the LLBLGen website in .net 45
-"C:\Program Files (x86)\Solutions Design\LLBLGen Pro v3.5\CliGenerator45.exe" "llbl\Northwind.Data.llblgenproj" "src" 0 "cligenerate.log"
-pause
