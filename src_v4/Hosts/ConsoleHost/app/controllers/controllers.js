43b9fe990d2f25e23791d540a7a1056a45110686
Matt Cowan <matthewj.cowan@gmail.com>
2013-07-09 23:52:55 -0500

    Fixed some performance issues with angularjs template, also a styling issue for mobile

--- src_v4/Hosts/ConsoleHost/app/controllers/controllers.js
+++ src_v4/Hosts/ConsoleHost/app/controllers/controllers.js
@@ -1,15 +1,16 @@
-app.controller('TypeCollectionController', function ($scope, $routeParams, metaService) {
+app.controller('TypeCollectionController', function ($scope, $routeParams, metaService, utilService) {
     $scope.name = 'TypeCollectionScope';
     $scope.types = []; $scope.typesLoaded = false;
     $scope.typeCollectionSlug = $routeParams.typeCollectionSlug.toLowerCase();
-    $scope.typeNameTCS = $scope.typeCollectionSlug == 'entities' ? 'Entity': ($scope.typeCollectionSlug == 'lists' ? 'Typed List': 'Typed View');
-    $scope.typeNameTCP = $scope.typeCollectionSlug == 'entities' ? 'Entities': ($scope.typeCollectionSlug == 'lists' ? 'Typed Lists': 'Typed Views');
+    $scope.typeNameTCS = $scope.typeCollectionSlug == 'entities' ? 'Entity' : ($scope.typeCollectionSlug == 'lists' ? 'Typed List' : 'Typed View');
+    $scope.typeNameTCP = $scope.typeCollectionSlug == 'entities' ? 'Entities' : ($scope.typeCollectionSlug == 'lists' ? 'Typed Lists' : 'Typed Views');
+    $scope.getBrowseName = function (item) { return $scope.typeCollectionSlug == 'entities' ? utilService.splitTitleCaseAndPluralize(item.name) : utilService.splitTitleCase(item.name); };
 
     init();
     function init() {
         metaService
             .getTypeCollection($routeParams.typeCollectionSlug.toLowerCase())
-            .then(function(data){$scope.types = data; $scope.typesLoaded = true;}, function(errorMessage){$scope.error=errorMessage;});
+            .then(function (data) { $scope.types = data; $scope.typesLoaded = true; }, function (errorMessage) { $scope.error = errorMessage; });
     };
 });
 
@@ -21,14 +22,15 @@ app.controller('TypeController', function ($scope, $location, $routeParams, meta
     $scope.currentTypeGridFields = [];
     $scope.typeCollectionSlug = $routeParams.typeCollectionSlug.toLowerCase();
     $scope.typeSlug = $routeParams.typeSlug.toLowerCase();
-    $scope.typeNameTCS = $scope.typeCollectionSlug == 'entities' ? 'Entity': ($scope.typeCollectionSlug == 'lists' ? 'Typed List': 'Typed View');
-    $scope.typeNameTCP = $scope.typeCollectionSlug == 'entities' ? 'Entities': ($scope.typeCollectionSlug == 'lists' ? 'Typed Lists': 'Typed Views');
+    $scope.typeNameTCS = $scope.typeCollectionSlug == 'entities' ? 'Entity' : ($scope.typeCollectionSlug == 'lists' ? 'Typed List' : 'Typed View');
+    $scope.typeNameTCP = $scope.typeCollectionSlug == 'entities' ? 'Entities' : ($scope.typeCollectionSlug == 'lists' ? 'Typed Lists' : 'Typed Views');
     $scope.filter = $location.search().filter || '';
     $scope.includes = $location.search().include || '';
     $scope.includesAvailable = [];
-    $scope.filterInput = ($location.search().filter && $location.search().filter.length > 0) ? ('filter=' + $location.search().filter): '';
+    $scope.filterInput = ($location.search().filter && $location.search().filter.length > 0) ? ('filter=' + $location.search().filter) : '';
     $scope.showingRelatedCount = $location.search().include && $location.search().include.length > 0;
-    $scope.showingRelatedCountText = ($location.search().include && $location.search().include.length > 0) ? 'Hide': 'Show';
+    $scope.showingRelatedCountText = ($location.search().include && $location.search().include.length > 0) ? 'Hide' : 'Show';
+    $scope.getBrowseName = function (item) { return $scope.typeCollectionSlug == 'entities' ? utilService.splitTitleCaseAndPluralize(item.name) : utilService.splitTitleCase(item.name); };
     //$scope.toSplitTitleCase = function(a){ return 'abc'; }
     $scope.editEnabled = false;
     $scope.deleteEnabled = false;
@@ -36,79 +38,81 @@ app.controller('TypeController', function ($scope, $location, $routeParams, meta
 
     //jQuery DataTable Variables
     $scope.dtSelectedRowData = null;
+    $scope.dtFirstLoad = true;
     $scope.dtReadyForInit = 0;
     $scope.dtAjaxColumns = [];
-    $scope.dtAjaxUrl = ($scope.typeCollectionSlug == 'entities' ? '/': ('/'+$scope.typeCollectionSlug+'/')) + $scope.typeSlug + '/datatable';
-    $scope.dtBuildAjaxUrl = function() {
+    $scope.dtAjaxUrl = ($scope.typeCollectionSlug == 'entities' ? '/' : ('/' + $scope.typeCollectionSlug + '/')) + $scope.typeSlug + '/datatable';
+    $scope.dtBuildAjaxUrl = function () {
         var baseUrl = $scope.dtAjaxUrl;
-        if($scope.filter.length>0) baseUrl += '?filter=' + $scope.filter;
-        if($scope.includes.length>0) baseUrl += ($scope.filter.length>0 ? '&': '?') + 'include=' + $scope.includes;
+        if ($scope.filter.length > 0) baseUrl += '?filter=' + $scope.filter;
+        if ($scope.includes.length > 0) baseUrl += ($scope.filter.length > 0 ? '&' : '?') + 'include=' + $scope.includes;
         return baseUrl;
     };
     $scope.dtDeletedEntity = null;
     $scope.dtRedraw = 0;
     $scope.dtRedrawAddEdit = 0;
 
-//    $scope.fieldIsSortableSearchableVisible = function(field){ return metaService.fieldIsSortableSearchableVisible(field); };
-//    $scope.uiBaseUrl = '/#/'+$routeParams.entityType.toLowerCase();
-//    $scope.ajaxBaseUrl = '/'+$routeParams.entityType.toLowerCase();
-//    $scope.applyFilter = function(){ var f = $('#filterText').val(); if(f) window.location = '/#/entities/' + $scope.currentType.metaLink.id + '?' + f; };
+    //    $scope.fieldIsSortableSearchableVisible = function(field){ return metaService.fieldIsSortableSearchableVisible(field); };
+    //    $scope.uiBaseUrl = '/#/'+$routeParams.entityType.toLowerCase();
+    //    $scope.ajaxBaseUrl = '/'+$routeParams.entityType.toLowerCase();
+    //    $scope.applyFilter = function(){ var f = $('#filterText').val(); if(f) window.location = '/#/entities/' + $scope.currentType.metaLink.id + '?' + f; };
 
     init();
     function init() {
         metaService
             .getTypeCollection($routeParams.typeCollectionSlug.toLowerCase())
-            .then(function(data){ $scope.types = data; $scope.typesLoaded = true; initType(); }, function(errorMessage){$scope.error=errorMessage;});
+            .then(function (data) { $scope.types = data; $scope.typesLoaded = true; initType(); }, function (errorMessage) { $scope.error = errorMessage; });
     };
     function initType() {
         metaService
             .getType($routeParams.typeCollectionSlug.toLowerCase(), $routeParams.typeSlug.toLowerCase())
-            .then(function(data){
-                for(var t=0;t<$scope.types.length;t++){
+            .then(function (data) {
+                for (var t = 0; t < $scope.types.length; t++) {
                     var ctype = $scope.types[t];
-                    if(ctype.metaLink.id.toLowerCase()==$scope.typeSlug){
+                    if (ctype.metaLink.id.toLowerCase() == $scope.typeSlug) {
                         $scope.currentType = ctype;
                         $scope.currentType.meta = data;
                         break;
                     }
                 }
-                if($scope.currentType.meta.includes){
-                for(var ic=0;ic<$scope.currentType.meta.includes.length;ic++){
-                    $scope.includesAvailable.push($scope.currentType.meta.includes[ic].id);
-                }}
+                if ($scope.currentType.meta.includes) {
+                    for (var ic = 0; ic < $scope.currentType.meta.includes.length; ic++) {
+                        $scope.includesAvailable.push($scope.currentType.meta.includes[ic].id);
+                    }
+                }
                 var dtColumns = [];
-                for(var fi=0;fi<$scope.currentType.meta.fields.length;fi++){
-                    var field=$scope.currentType.meta.fields[fi];
-                    if(field.properties.relation) continue;
+                for (var fi = 0; fi < $scope.currentType.meta.fields.length; fi++) {
+                    var field = $scope.currentType.meta.fields[fi];
+                    if (field.properties.relation) continue;
                     var finfo = metaService.fieldIsSortableSearchableVisible(field);
                     field.isVisible = finfo[2];
                     field.displayName = utilService.toSplitTitleCase(field.id);
                     $scope.currentTypeGridFields.push(field);
                     dtColumns.push({ "bSortable": finfo[0], "bSearchable": finfo[1], "bVisible": finfo[2] });
                 }
-                if($scope.typeCollectionSlug == "entities")
+                if ($scope.typeCollectionSlug == "entities")
                     dtColumns.push({ "bSortable": false, "bSearchable": false });
                 $scope.dtAjaxColumns = dtColumns;
                 $scope.currentTypeLoaded = true;
                 $scope.dtReadyForInit = $scope.dtReadyForInit + 1;
-            }, function(errorMessage){$scope.error=errorMessage;});
+            }, function (errorMessage) { $scope.error = errorMessage; });
     };
 
     //Show / Hide a Column (call this from within template)
     $scope.dtToggleColumnIndex = ""; //bind this to datatable directive attribute for immediate datagrid refresh
-    $scope.showHideColumn = function($event, iColIdx) {
+    $scope.showHideColumn = function ($event, iColIdx) {
         var checkbox = $event.target;
         var action = (checkbox.checked ? 'add' : 'remove');
         $scope.dtToggleColumnIndex = iColIdx + "," + action;
     };
     //Determine if a column is visible (call this from within template)
-    $scope.fieldIsVisible = function(field){ return metaService.fieldIsSortableSearchableVisible(field)[2]; };
+    $scope.fieldIsVisible = function (field) { return metaService.fieldIsSortableSearchableVisible(field)[2]; };
 
-    $scope.showRelatedCount = function(){
+    $scope.showRelatedCount = function () {
         var s = {};
-        if ($scope.filter.length>0)s.filter=$scope.filter;
+        if ($scope.filter.length > 0) s.filter = $scope.filter;
         if (!$scope.showingRelatedCount) {
-            if($scope.includesAvailable.length>0)s.include=$scope.includesAvailable;
+            if ($scope.includesAvailable.length > 0) s.include = $scope.includesAvailable;
         }
         $location.search(s).path('/' + $scope.typeCollectionSlug + '/' + $scope.typeSlug);
     };
@@ -119,25 +123,25 @@ app.controller('TypeController', function ($scope, $location, $routeParams, meta
         $scope.modalError = null;
         $scope.modalTitle = ae + ' ' + $scope.splitTitleCase($scope.currentType.name);
         $scope.modalAction = '/' + $scope.typeSlug;
-        if($scope.dtSelectedRowData != null && ae == 'Edit') {
+        if ($scope.dtSelectedRowData != null && ae == 'Edit') {
             $scope.modalAction += '/' + typeService.buildPkString($scope.dtSelectedRowData, $scope.currentType.meta.fields) + '/update';
             //populate form with actual data from db (row data may not be fully populated)
             typeService
                 .fetchItem($scope.dtSelectedRowData, $scope.typeSlug, $scope.currentType.meta.fields)
                 .then(
-                    function(data){ $scope.dtShowAddEditItemModal(data);}, 
-                    function(error){$scope.error=error;});
+                    function (data) { $scope.dtShowAddEditItemModal(data); },
+                    function (error) { $scope.error = error; });
         } else {
             $scope.dtShowAddEditItemModal(null);
         }
     };
-    $scope.dtShowAddEditItemModal = function(data) {
+    $scope.dtShowAddEditItemModal = function (data) {
         $scope.selectedEntity = data;
         $scope.dtRedrawAddEdit = $scope.dtRedrawAddEdit + 1; //forces refresh of form fields
         $("#addEditItemModal").modal('show');
-        $("#btnSubmitAddEditForm").click(function() {/*event.preventDefault();*/ $scope.dtDoAddEditItem(); return false;});
+        $("#btnSubmitAddEditForm").click(function () {/*event.preventDefault();*/ $scope.dtDoAddEditItem(); return false; });
     };
-    $scope.dtDoAddEditItem = function() {
+    $scope.dtDoAddEditItem = function () {
         // Use the form plugin
         var $form = $('#addEditItemForm');
         var $target = $($form.attr('data-target'));
@@ -148,14 +152,14 @@ app.controller('TypeController', function ($scope, $location, $routeParams, meta
             dataType: 'json',
             target: $target,
             timeout: 600000,
-            success: function(data){
+            success: function (data) {
                 $target.modal('hide');
                 $scope.$apply(function () {
-                    $scope.dtRedraw = $scope.dtRedraw+1;
+                    $scope.dtRedraw = $scope.dtRedraw + 1;
                 });
-            }, 
-            error: function(xhr, desc, exceptionobj){
-                var errors = utilService.errorMessages(xhr,desc,exceptionobj);
+            },
+            error: function (xhr, desc, exceptionobj) {
+                var errors = utilService.errorMessages(xhr, desc, exceptionobj);
                 $scope.$apply(function () {
                     $scope.modalError = '<ul><li>' + errors.join('</li><li>') + '</li></ul>';
                 });
@@ -165,12 +169,12 @@ app.controller('TypeController', function ($scope, $location, $routeParams, meta
     }
     $scope.dtDeleteItem = function () {
         $scope.error = null;
-        typeService.deleteItem($scope.dtSelectedRowData, $scope.typeSlug, $scope.currentType.meta.fields).then(function(){
-           $scope.dtDeletedEntity = $scope.dtSelectedRowData;
-           $scope.dtSelectedRowData = null;
-           $scope.selectedEntity = null;
+        typeService.deleteItem($scope.dtSelectedRowData, $scope.typeSlug, $scope.currentType.meta.fields).then(function () {
+            $scope.dtDeletedEntity = $scope.dtSelectedRowData;
+            $scope.dtSelectedRowData = null;
+            $scope.selectedEntity = null;
         },
-        function(errorMessage){$scope.error=errorMessage;});
+        function (errorMessage) { $scope.error = errorMessage; });
     };
 
     // DataTable Directive Variables
@@ -182,17 +186,17 @@ app.controller('TypeController', function ($scope, $location, $routeParams, meta
         $("#entity-grid").width("100%");
         return settings;
     };
-    $scope.dtRowCallback = function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+    $scope.dtRowCallback = function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
         // rewrite href to match angular spa format
-        $('td:last a', nRow).each(function(){
+        $('td:last a', nRow).each(function () {
             var href = $(this).attr("href");
-            if(href && href.length>0) {
+            if (href && href.length > 0) {
                 $(this).attr("href", '/#/entities' + href);
             };
         });
 
-        $(nRow).bind('click', function() {
-            $scope.$apply(function() {
+        $(nRow).bind('click', function () {
+            $scope.$apply(function () {
                 var r = $(nRow);
                 var isSelected = r.hasClass('entity-selected');
                 if (isSelected) {
@@ -212,17 +216,28 @@ app.controller('TypeController', function ($scope, $location, $routeParams, meta
         });
         return nRow;
     };
-    $scope.dtServerParams = function ( aoData ) {
+    $scope.dtServerParams = function (aoData) {
         var iSelectColumns = [];
-        $("#dt_d_nav li:not(:last-child) input:checked").each(function(){
-            iSelectColumns.push($(this).attr("value"));
-        });
-        aoData.push( { "name": "iSelectColumns", "value": iSelectColumns } );
+        if ($scope.dtFirstLoad) {
+            //on first load, use grid fields to initialize datatable
+            for (var i = 0; i < $scope.currentTypeGridFields.length; i++) {
+                var f = $scope.currentTypeGridFields[i];
+                if (f.isVisible) {
+                    iSelectColumns.push(f.properties.index);
+                }
+            }
+            $scope.dtFirstLoad = false;
+        } else {
+            $("#dt_d_nav li:not(:last-child) input:checked").each(function () {
+                iSelectColumns.push($(this).attr("value"));
+            });
+        }
+        aoData.push({ "name": "iSelectColumns", "value": iSelectColumns });
     };
     $scope.dtOverrideOptions = {
         "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
         "sPaginationType": "bootstrap",
-        "oLanguage": {"sZeroRecord": "Please wait - fetching data", "sLengthMenu": "_MENU_ records per page"},
+        "oLanguage": { "sZeroRecord": "Please wait - fetching data", "sLengthMenu": "_MENU_ records per page" },
         "sAjaxSource": $scope.dtBuildAjaxUrl(),
         "bProcessing": false,
         "bServerSide": true,
--- src_v4/Hosts/ConsoleHost/app/services/services.js
+++ src_v4/Hosts/ConsoleHost/app/services/services.js
@@ -1,72 +1,74 @@
-app.factory('metaService', function($http, $q){
+app.factory('metaService', function ($http, $q) {
     return {
-        getTypeCollection: function(typeCollectionSlug){
+        getTypeCollection: function (typeCollectionSlug) {
             var deferred = $q.defer();
-            var metaUrl = '/' + typeCollectionSlug;
+            var metaUrl = '/' + typeCollectionSlug + '?v=' + Math.floor(Math.random() * 999);
             $http
                 .get(metaUrl)
-                .success(function(data){deferred.resolve(data.result);})
-                .error(function(){deferred.reject("An error occured while fetching " + metaUrl);});
+                .success(function (data) { deferred.resolve(data.result); })
+                .error(function () { deferred.reject("An error occured while fetching " + metaUrl); });
             return deferred.promise;
         },
-        getType: function(typeCollectionSlug, typeSlug){
+        getType: function (typeCollectionSlug, typeSlug) {
             var deferred = $q.defer();
-            var metaUrl = (typeCollectionSlug == 'entities' ? '': ('/' + typeCollectionSlug)) + '/' + typeSlug + '/meta';
+            var metaUrl = (typeCollectionSlug == 'entities' ? '' : ('/' + typeCollectionSlug)) + '/' + typeSlug + '/meta';
             $http
                 .get(metaUrl)
-                .success(function(data){deferred.resolve(data);})
-                .error(function(){deferred.reject("An error occured while fetching metadata for " + metaUrl);});
+                .success(function (data) { deferred.resolve(data); })
+                .error(function () { deferred.reject("An error occured while fetching metadata for " + metaUrl); });
             return deferred.promise;
         },
         //returns array: [sortable, searchable, visible]
-        fieldIsSortableSearchableVisible:  function(field){
-            if(field.type == 'String') { var ml=parseInt(field.properties.maxLength); return [ml<255, ml<1000, ml<128 && !field.properties.isNullable]; }
-            else if(field.type == 'Int32') { return [true, true, !field.properties.isNullable]; }
+        fieldIsSortableSearchableVisible: function (field) {
+            if (field.type == 'String') { var ml = parseInt(field.properties.maxLength); return [ml < 255, ml < 1000, ml < 128 && !field.properties.isNullable]; }
+            else if (field.type == 'Int32') { return [true, true, !field.properties.isNullable]; }
             else { return [false, false, false]; }
         }
     };
 });
 
-app.factory('typeService', function($http, $q){
+app.factory('typeService', function ($http, $q) {
     return {
-        fetchItem: function(rowData, typeSlug, fields){
+        fetchItem: function (rowData, typeSlug, fields) {
             var pkUrl = this.buildPkString(rowData, fields);
             var deferred = $q.defer();
             $http
-                .get('/'+typeSlug+'/'+pkUrl)
-                .success(function(data){deferred.resolve(data.result);})
-                .error(function(e,s,d){
-                    var message = 'Unable to fetch item at ' + '/'+ typeSlug + '/' +pkUrl;
-                    deferred.reject(message)});
+                .get('/' + typeSlug + '/' + pkUrl)
+                .success(function (data) { deferred.resolve(data.result); })
+                .error(function (e, s, d) {
+                    var message = 'Unable to fetch item at ' + '/' + typeSlug + '/' + pkUrl;
+                    deferred.reject(message)
+                });
             return deferred.promise;
         },
-        deleteItem: function(rowData, typeSlug, fields){
+        deleteItem: function (rowData, typeSlug, fields) {
             var pkUrl = this.buildPkString(rowData, fields);
             var deferred = $q.defer();
             $http
-                .post('/'+typeSlug+'/'+pkUrl+'/delete')
-                .success(function(data){deferred.resolve(data);})
-                .error(function(e,s,d){
-                    var message = (e && e.length>0) ? e: (s == 401 ? "Sorry, you're not authorized to delete items.": "An error occured while attempting to delete this item");
-                    deferred.reject(message)});
+                .post('/' + typeSlug + '/' + pkUrl + '/delete')
+                .success(function (data) { deferred.resolve(data); })
+                .error(function (e, s, d) {
+                    var message = (e && e.length > 0) ? e : (s == 401 ? "Sorry, you're not authorized to delete items." : "An error occured while attempting to delete this item");
+                    deferred.reject(message)
+                });
             return deferred.promise;
         },
-        buildPkString: function(rowData, fields){
+        buildPkString: function (rowData, fields) {
             var pks = [];
-            for(var fi=0;fi<fields.length;fi++){
-              var f = fields[fi];
-              var fp = f.properties;
-              if(fp.isPrimaryKey){
-                var fpidx = fp['index'];
-                pks.push(rowData[fpidx]);
-              }
+            for (var fi = 0; fi < fields.length; fi++) {
+                var f = fields[fi];
+                var fp = f.properties;
+                if (fp.isPrimaryKey) {
+                    var fpidx = fp['index'];
+                    pks.push(rowData[fpidx]);
+                }
             }
             return pks.join('/');
         }
     };
 });
 
-app.factory('utilService', function($http, $q){
+app.factory('utilService', function ($http, $q) {
 
     //pluralize/singularize (first setup edge cases)
     owl.pluralize.define("demo", "demos");
@@ -75,43 +77,43 @@ app.factory('utilService', function($http, $q){
 
     return {
         //StringService(s)
-        capitalize: function(str){return str.replace(/^./, function (char) {return char.toUpperCase();});},
-        pluralize: function(str){if(!str){return '';} return owl.pluralize(str);},
-        singularize: function(str){if(!str){return '';} return owl.pluralize(str,1);},
+        capitalize: function (str) { return str.replace(/^./, function (char) { return char.toUpperCase(); }); },
+        pluralize: function (str) { if (!str) { return ''; } return owl.pluralize(str); },
+        singularize: function (str) { if (!str) { return ''; } return owl.pluralize(str, 1); },
         //string functions
-        removeSpaces: function(str){if(!str){return '';} return str.split(' ').join('');},
-        splitTitleCase: function(str){ if(!str){return '';} return str.replace(/([a-z])([A-Z]|[0-9])/g, '$1 $2'); },
-        splitTitleCaseAndPluralize: function(str){if(!str){return '';} var tc = this.splitTitleCase(str).split(' '); tc[tc.length-1]=this.pluralize(tc[tc.length-1]); return tc.join(' '); },
+        removeSpaces: function (str) { if (!str) { return ''; } return str.split(' ').join(''); },
+        splitTitleCase: function (str) { if (!str) { return ''; } return str.replace(/([a-z])([A-Z]|[0-9])/g, '$1 $2'); },
+        splitTitleCaseAndPluralize: function (str) { if (!str) { return ''; } var tc = this.splitTitleCase(str).split(' '); tc[tc.length - 1] = this.pluralize(tc[tc.length - 1]); return tc.join(' '); },
         //toTitleCase: https://github.com/gouch/to-title-case/blob/master/to-title-case.js (minified with yui)
-        toTitleCase: function(str){if(!str){return '';} var a=/^(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|vs?\.?|via)$/i;var str2 = str.replace(/([^\W_]+[^\s-]*) */g,function(c,e,b,d){if(b>0&&b+e.length!==d.length&&e.search(a)>-1&&d.charAt(b-2)!==":"&&d.charAt(b-1).search(/[^\s-]/)<0){return c.toLowerCase();}if(e.substr(1).search(/[A-Z]|\../)>-1){return c;}return c.charAt(0).toUpperCase()+c.substr(1);}); return this.capitalize(str2);},
-        toSplitTitleCase: function(str){if(!str){return '';} return this.splitTitleCase(this.toTitleCase(str));},
+        toTitleCase: function (str) { if (!str) { return ''; } var a = /^(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|vs?\.?|via)$/i; var str2 = str.replace(/([^\W_]+[^\s-]*) */g, function (c, e, b, d) { if (b > 0 && b + e.length !== d.length && e.search(a) > -1 && d.charAt(b - 2) !== ":" && d.charAt(b - 1).search(/[^\s-]/) < 0) { return c.toLowerCase(); } if (e.substr(1).search(/[A-Z]|\../) > -1) { return c; } return c.charAt(0).toUpperCase() + c.substr(1); }); return this.capitalize(str2); },
+        toSplitTitleCase: function (str) { if (!str) { return ''; } return this.splitTitleCase(this.toTitleCase(str)); },
         //switch themes
-        changeTheme: function(theme){if(!theme){return;} $.cookie('Theme', theme); window.location = '/'; },
+        changeTheme: function (theme) { if (!theme) { return; } $.cookie('Theme', theme); window.location = '/'; },
 
         //ServiceStack Utility Service Calls
         //From: https://github.com/ServiceStack/ServiceStack/blob/master/release/latest/js/JsonServiceClient.js
-        errorMessages: function(xhr, desc, exceptionobj) {
+        errorMessages: function (xhr, desc, exceptionobj) {
             var errors = [];
-            try{
+            try {
                 var response = this.parseJSON(xhr.responseText);
                 var status = this.parseResponseStatus(response);
-                if(status.message && status.message.length>0){
+                if (status.message && status.message.length > 0) {
                     errors.push(status.message);
                 }
-                if(status.fieldErrors && status.fieldErrors.length>0){
+                if (status.fieldErrors && status.fieldErrors.length > 0) {
                     for (var i = 0, len = status.fieldErrors.length; i < len; i++) {
                         var errMessage = status.fieldErrors[i].errMessage;
                         errors.push(errMessage);
                     }
                 }
             }
-            catch(e){}
-            if(errors.length==0){
-                errors.push(xhr.responseText ? xhr.responseText: (desc ? desc: 'Unknown error'));
+            catch (e) { }
+            if (errors.length == 0) {
+                errors.push(xhr.responseText ? xhr.responseText : (desc ? desc : 'Unknown error'));
             }
             return errors;
         },
-        parseJSON: function(json) {
+        parseJSON: function (json) {
             if (typeof (JSON) == 'object' && JSON.parse)
                 return JSON.parse(json);
             if ($ && $.parseJSON)
@@ -120,7 +122,7 @@ app.factory('utilService', function($http, $q){
                 return goog.json.parse(json);
             throw "no json parser found";
         },
-        parseResponseStatus: function(status) {
+        parseResponseStatus: function (status) {
 
             //ResponseStatus class
             //https://github.com/ServiceStack/ServiceStack/blob/master/src/ServiceStack.Interfaces/ServiceInterface.ServiceModel/ResponseStatus.cs
